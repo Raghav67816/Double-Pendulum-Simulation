4a2fb6bdabe36641e37616e7ca64ce028c8df84a
Raghav67816 <kumaraghav079@gmail.com>
2025-11-14 12:31:42 +0530

    added readme and screenshot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from math import radians, sin, cos
+from math import radians, sin, cos, dist, atan2
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -9,6 +9,8 @@ G = 9.8
 
 class DoublePendulum:
     def __init__(self):
+        super(DoublePendulum, self).__init__()
+
         self.origin_x = 400
         self.origin_y = 100
 
@@ -18,14 +20,41 @@ class DoublePendulum:
         self.mass_bob_a = 10
         self.mass_bob_b = 10
 
-        self.theta_1 = radians(120)
-        self.theta_2 = radians(120)
+        self.theta_1 = radians(0)
+        self.theta_2 = radians(0)
 
         self.omega_a = 0
         self.omega_b = 0
         self.prev_cords = []
 
         self.update_cords()
+
+    def get_theta_2(self):
+        return self.theta_2
+
+    def reset_to_default(self):
+        self.origin_x = 400
+        self.origin_y = 100
+
+        self.length_rod_1 = 120
+        self.length_rod_2 = 120
+
+        self.mass_bob_a = 10
+        self.mass_bob_b = 10
+
+        self.theta_1 = radians(0)
+        self.theta_2 = radians(0)
+
+        self.omega_a = 0
+        self.omega_b = 0
+
+    def manual_update_bob_b_cords(self, initial_bob_pos: tuple, new_mouse_pos: tuple):
+        delta_x = new_mouse_pos[0] - self.x2
+        delta_y = new_mouse_pos[1] - self.y2
+
+        new_theta = atan2(-delta_y, delta_x)
+        self.theta_2 = new_theta
+        
 
     def update_cords(self):
         self.x1 = self.origin_x + self.length_rod_1 * sin(self.theta_1)
@@ -72,6 +101,7 @@ class DoublePendulum:
 
 
 dp = DoublePendulum()
+is_bob_selected = False
 
 running = True
 clock = pygame.time.Clock()
@@ -82,9 +112,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if dist(mouse_pos, (dp.x2, dp.y2)) <= 10:
+                is_bob_selected = True
+                
+        if event.type == pygame.MOUSEBUTTONUP and is_bob_selected == True:
+            is_bob_selected = False
+            dp.manual_update_bob_b_cords((dp.x2, dp.y2), pygame.mouse.get_pos())
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                dp.reset_to_default()
+                dp.prev_cords = []
+            
+
     dp.step(0.05)
 
     screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 30)
     cords = dp.get_cords()
 
     pygame.draw.circle(screen, (0, 0, 255), (dp.origin_x, dp.origin_y), 10)
@@ -101,7 +147,13 @@ while running:
         pygame.draw.aalines(
             screen, (0, 255, 0), False, dp.prev_cords
         )
+    text = font.render(f"X2: {round(dp.x2, 2)}, Y2: {round(dp.y2, 2)}", True, (255, 255, 255), (0,0,0))
+    screen.blit(
+        text,
+        (50, 50)
+    )
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(30)
 
 pygame.quit()
+    
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Double Pendulum Simulation
+
+An exciting simulation of Double Pendulum. It's just so cool to simulate physics stuff.
+
+![alt text](image.png)
+
+### Installation
+- Download the latest release
+- Execute the file
+
+### How to use
+
+Hold the green bob and drag it, you won't see it dragging but it does drag. Leave the mouse when you want and let it swing. and ENJOY
+
+Press R to reset.
